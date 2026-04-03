0c8a32048fb92e7e6c8afe1e5d4cb682f1e9d885
thisarasathsara63-sudo <thisarasathsara63@gmail.com>
2026-04-03 09:07:08 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'thisarasathsara63@gmail.com',
+    password: '_UiFtf9NFAR-5Sf',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
